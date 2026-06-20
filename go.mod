b5857316e5a3c2c894181191afe63ef4d851da18
Colan Biemer <bi3mer93@gmail.com>
2026-06-20 17:49:26 -0500

    Chapter 2 Done

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module go-kilo
 go 1.26.3
 
 require (
-	golang.org/x/sys v0.46.0 // indirect
-	golang.org/x/term v0.44.0 // indirect
+	golang.org/x/sys v0.46.0
+	golang.org/x/term v0.44.0
 )
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"go-kilo/internal"
 	"os"
 
+	"golang.org/x/sys/unix"
 	"golang.org/x/term"
 )
 
@@ -12,32 +13,63 @@ func main() {
 	// ---------------------------------------------------------------------------
 	// Enable raw mode (i.e. character input doesn't echo) using Go's library
 	// term rather than setting the flags ourselves.
-	oldFD, err := term.MakeRaw(int(os.Stdin.Fd())) // fd -> file descriptor
+	fd := int(os.Stdin.Fd()) // file descriptor
+	oldState, err := term.MakeRaw(fd)
 	if err != nil {
 		fmt.Printf("Unable to interact with terminal: %s\n", err)
 		return
 	}
 
 	// Diable raw mode after the program quits. Will still be called on error.
-	defer term.Restore(int(os.Stdin.Fd()), oldFD)
+	defer term.Restore(fd, oldState)
+
+	// ---------------------------------------------------------------------------
+	// grab the current termios so we can update on failed reads
+	t, err := unix.IoctlGetTermios(fd, internal.GetTermios)
+	if err != nil {
+		fmt.Printf("unix.IoctlGetTermios error: %s\n", err)
+		return
+	}
+
+	t.Cc[unix.VMIN] = 0  // min bytes to read from user
+	t.Cc[unix.VTIME] = 1 // max time before reading from user returns
+
+	if err := unix.IoctlSetTermios(fd, internal.SetTermios, t); err != nil {
+		fmt.Printf("unix.IoctlSetTermios error: %s\n", err)
+		return
+	}
 
 	// ---------------------------------------------------------------------------
 	// Reader user input, byte-by-byte
-	reader := bufio.NewReader(os.Stdin)
-
+	buf := make([]byte, 1)
 	for {
-		userInput, err := reader.ReadByte()
-
+		// get user input
+		numBytes, err := unix.Read(fd, buf)
 		if err != nil {
 			fmt.Printf("Encountered error: %s\n", err)
 			return
 		}
 
-		if userInput == 'q' {
-			fmt.Println("Bye!")
-			return
+		// If numBytes == 0, then time since user input exceeded unix.VTIME max.
+		// So we don't process input and end the loop.
+		if numBytes == 0 {
+			continue
 		}
 
-		fmt.Printf("Char: %c\r\n", userInput)
+		// Read input
+		c := buf[0]
+
+		// ctrl + q quits
+		if c == 17 {
+			fmt.Print("Bye!\r\n")
+			break
+		}
+
+		// otherwise just print some stuff
+		if c < 32 || c == 127 { // ctrl + key
+			fmt.Printf("%d\r\n", c)
+		} else {
+			fmt.Printf("%d ('%c')\r\n", c, c)
+		}
 	}
 }
